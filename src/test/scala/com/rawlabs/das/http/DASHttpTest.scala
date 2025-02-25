7e0ad0ce155b31e4b55bdcdf9b7d9ae221d4bf96
cesar <cesar.matos@gmail.com>
2025-02-25 16:16:34 +0100

    Applying format

--- src/test/scala/com/rawlabs/das/http/DASHttpTest.scala
+++ src/test/scala/com/rawlabs/das/http/DASHttpTest.scala
@@ -1,60 +1,54 @@
 package com.rawlabs.das.http
 
+import scala.jdk.CollectionConverters._
+
 import org.scalatest.funsuite.AnyFunSuite
 
-import com.rawlabs.protocol.das.v1.tables.{Row => ProtoRow}
-import com.rawlabs.protocol.das.v1.query.{
-  Qual => ProtoQual,
-  SimpleQual,
-  Operator
-}
-import com.rawlabs.protocol.das.v1.types.{
-  Value => ProtoValue,
-  ValueString,
-  ValueBool,
-  ValueRecord,
-  ValueRecordAttr
-}
 import com.rawlabs.das.sdk.DASSdkException
-
-import scala.jdk.CollectionConverters._
+import com.rawlabs.protocol.das.v1.query.{Operator, Qual => ProtoQual, SimpleQual}
+import com.rawlabs.protocol.das.v1.tables.{Row => ProtoRow}
+import com.rawlabs.protocol.das.v1.types.{Value => ProtoValue, ValueBool, ValueRecord, ValueRecordAttr, ValueString}
 
 /**
  * Unit tests for DASHttpTable which:
- *  - Requires 'url' in WHERE
- *  - Enforces type checking for columns
- *  - Throws if unrecognized column or operator != EQUALS
- *  - Produces 1 row with columns: response_status_code, response_body, etc.
+ *   - Requires 'url' in WHERE
+ *   - Enforces type checking for columns
+ *   - Throws if unrecognized column or operator != EQUALS
+ *   - Produces 1 row with columns: response_status_code, response_body, etc.
  */
 class DASHttpTableTest extends AnyFunSuite {
 
   // A helper method to create a SimpleQual with (column = stringValue)
   private def qualString(colName: String, strVal: String): ProtoQual = {
-    val strValue = ProtoValue.newBuilder()
+    val strValue = ProtoValue
+      .newBuilder()
       .setString(ValueString.newBuilder().setV(strVal))
 
-    ProtoQual.newBuilder()
+    ProtoQual
+      .newBuilder()
       .setName(colName)
       .setSimpleQual(
-        SimpleQual.newBuilder()
+        SimpleQual
+          .newBuilder()
           .setOperator(Operator.EQUALS)
-          .setValue(strValue)
-      )
+          .setValue(strValue))
       .build()
   }
 
   // A helper method to create a SimpleQual with (column = boolValue)
   private def qualBool(colName: String, boolVal: Boolean): ProtoQual = {
-    val boolValue = ProtoValue.newBuilder()
+    val boolValue = ProtoValue
+      .newBuilder()
       .setBool(ValueBool.newBuilder().setV(boolVal))
 
-    ProtoQual.newBuilder()
+    ProtoQual
+      .newBuilder()
       .setName(colName)
       .setSimpleQual(
-        SimpleQual.newBuilder()
+        SimpleQual
+          .newBuilder()
           .setOperator(Operator.EQUALS)
-          .setValue(boolValue)
-      )
+          .setValue(boolValue))
       .build()
   }
 
@@ -63,24 +57,22 @@ class DASHttpTableTest extends AnyFunSuite {
   private def qualRecordOfStrings(colName: String, kvPairs: Map[String, String]): ProtoQual = {
     val recordBuilder = ValueRecord.newBuilder()
     kvPairs.foreach { case (k, v) =>
-      val valAttr = ValueRecordAttr.newBuilder()
+      val valAttr = ValueRecordAttr
+        .newBuilder()
         .setName(k)
-        .setValue(
-          ProtoValue.newBuilder().setString(
-            ValueString.newBuilder().setV(v)
-          )
-        )
+        .setValue(ProtoValue.newBuilder().setString(ValueString.newBuilder().setV(v)))
       recordBuilder.addAtts(valAttr)
     }
     val recordValue = ProtoValue.newBuilder().setRecord(recordBuilder)
 
-    ProtoQual.newBuilder()
+    ProtoQual
+      .newBuilder()
       .setName(colName)
       .setSimpleQual(
-        SimpleQual.newBuilder()
+        SimpleQual
+          .newBuilder()
           .setOperator(Operator.EQUALS)
-          .setValue(recordValue)
-      )
+          .setValue(recordValue))
       .build()
   }
 
@@ -98,7 +90,7 @@ class DASHttpTableTest extends AnyFunSuite {
   private def rowToMap(row: ProtoRow): Map[String, String] = {
     row.getColumnsList.asScala.map { c =>
       val name = c.getName
-      val v    = c.getData
+      val v = c.getData
       val s =
         if (v.hasString) v.getString.getV
         else if (v.hasBool) v.getBool.getV.toString
@@ -126,11 +118,8 @@ class DASHttpTableTest extends AnyFunSuite {
 
   test("Unknown column => throws DASSdkException") {
     val table = new DASHttpTable()
-    val quals = Seq(
-      qualString("url", "http://example.com"),
-      qualString("method", "GET"),
-      qualBool("some_unknown_col", true)
-    )
+    val quals =
+      Seq(qualString("url", "http://example.com"), qualString("method", "GET"), qualBool("some_unknown_col", true))
 
     assertThrows[DASSdkException] {
       table.execute(quals, Seq.empty, Seq.empty, None)
@@ -140,16 +129,16 @@ class DASHttpTableTest extends AnyFunSuite {
   test("Wrong operator => throws DASSdkException") {
     val table = new DASHttpTable()
     // We'll build a Qual with operator = LESS_THAN
-    val q = ProtoQual.newBuilder()
+    val q = ProtoQual
+      .newBuilder()
       .setName("url")
       .setSimpleQual(
-        SimpleQual.newBuilder()
-          .setOperator(Operator.LESS_THAN)  // not EQUALS => should throw
-          .setValue(
-            ProtoValue.newBuilder()
-              .setString(ValueString.newBuilder().setV("http://example.com"))
-          )
-      )
+        SimpleQual
+          .newBuilder()
+          .setOperator(Operator.LESS_THAN) // not EQUALS => should throw
+          .setValue(ProtoValue
+            .newBuilder()
+            .setString(ValueString.newBuilder().setV("http://example.com"))))
       .build()
 
     assertThrows[DASSdkException] {
@@ -196,12 +185,7 @@ class DASHttpTableTest extends AnyFunSuite {
 
     val quals = Seq(qUrl, qMethod, qBody, qRedirect, qHeaders, qArgs)
 
-    val result = table.execute(
-      quals,
-      Seq("url","method","response_status_code","response_body"),
-      Seq.empty,
-      None
-    )
+    val result = table.execute(quals, Seq("url", "method", "response_status_code", "response_body"), Seq.empty, None)
     val rows = collectRows(result)
     assert(rows.size == 1)
 
@@ -222,20 +206,17 @@ class DASHttpTableTest extends AnyFunSuite {
     val qBody = qualString("request_body", """{"foo":"bar"}""")
     val qRedirect = qualBool("follow_redirect", true)
 
-    val qHeaders = qualRecordOfStrings("request_headers", Map(
-      "Content-Type" -> "application/json",
-      "User-Agent"   -> "MyDAS"
-    ))
-    val qArgs = qualRecordOfStrings("url_args", Map("debug" -> "true","test"->"123"))
+    val qHeaders =
+      qualRecordOfStrings("request_headers", Map("Content-Type" -> "application/json", "User-Agent" -> "MyDAS"))
+    val qArgs = qualRecordOfStrings("url_args", Map("debug" -> "true", "test" -> "123"))
 
     val quals = Seq(qUrl, qMethod, qBody, qRedirect, qHeaders, qArgs)
 
     val result = table.execute(
       quals,
-      Seq("url","request_headers","url_args","follow_redirect","response_status_code","response_body"),
+      Seq("url", "request_headers", "url_args", "follow_redirect", "response_status_code", "response_body"),
       Seq.empty,
-      None
-    )
+      None)
     val rows = collectRows(result)
     assert(rows.size == 1)
 
